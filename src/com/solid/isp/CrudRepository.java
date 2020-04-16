cec2b7c59e0f9edeabf61df01d8b81636e6b4356
Duc <vanduc2514@gmail.com>
2020-04-16 23:59:24 +0700

    Sửa lại chú thích của nguyên lý thứ 4 - Interface Segregation Principle

--- src/com/solid/isp/CrudRepository.java
+++ src/com/solid/isp/CrudRepository.java
@@ -1,5 +1,11 @@
 package com.solid.isp;
 
+/*
+* Interface này được tách ra từ Interface Repository do đó nếu sử dụng Interface này chỉ cần triển khai
+các phương thức của Interface này mà không cần phải triển khai toàn bộ các phương thức của Interface
+Repository
+*/
+
 public interface CrudRepository<T,ID> {
     Iterable<T> findAll();
 
